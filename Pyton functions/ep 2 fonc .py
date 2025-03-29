d5f7093c1d0d6ee0e548df7fea70077590c2e37c
amine-Xytone <amineeljohri1@gmail.com>
2025-03-29 06:50:07 +0000

    ep 2

---
+++ Pyton functions/ep 2 fonc .py
@@ -0,0 +1,26 @@
+# en general il y a 4 types des fonctions 
+# 1 .
+def hello ():
+    print("helllo mr how are you !")
+
+hello()
+
+
+# 2 .
+def hello_2 (nom):
+    print(f"hello mr {nom} how are you !")
+
+hello_2("amine")
+
+
+# 3.
+def avec_rutourn_pas_dargument() :
+    a = 1
+    b = 3 
+    c = a + b
+    return c
+
+# 4.
+def avec_rutourn_dargument(a,b) :
+    c = a + b
+    return c
